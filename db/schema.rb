488fac4dcd13284ca798c0384178efd543aad029
Tu Anh <thotrang1712@gmail.com>
2016-04-03 04:54:44 +0000

    ok bootstraps

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160403024656) do
+ActiveRecord::Schema.define(version: 20160403033758) do
 
   create_table "comments", force: :cascade do |t|
     t.string   "user_name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20160403024656) do
     t.integer  "idea_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "like_count"
   end
 
   create_table "ideas", force: :cascade do |t|
